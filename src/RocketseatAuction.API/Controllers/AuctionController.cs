e4f02a553affee3fb361b8660032c6b4ec5690d3
jeana <jeana@JEAN_PC>
2024-02-05 23:07:05 -0300

    Adicionar arquivos de projeto.

---
+++ src/RocketseatAuction.API/Controllers/AuctionController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RocketseatAuction.API.Entities;
+using RocketseatAuction.API.UseCases.Auctions.GetCurrent;
+
+namespace RocketseatAuction.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuctionController : ControllerBase
+{
+    [HttpGet]
+    [ProducesResponseType(typeof(Auction), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public IActionResult GetCurrentAuction()
+    {
+        var useCase = new GetCurrentAuctionsUseCase();
+        var result = useCase.Execute();
+
+        if (result is null)
+            return NoContent();
+
+        return Ok(result);
+    }
+
+    [HttpPost]
+    public IActionResult CreateAuction()
+    {
+        return Created();
+    }
+
+    [HttpPut]
+    public IActionResult UpdateAuction() 
+    {
+        return Ok();
+    }
+
+    [HttpDelete]
+    public IActionResult DeleteAuction()
+    {
+        return Ok();
+    }
+}
---
+++ src/RocketseatAuction.API/UseCases/Auctions/GetCurrent/GetCurrentAuctionsUseCase.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RocketseatAuction.API.Entities;
+using RocketseatAuction.API.Repositories;
+
+namespace RocketseatAuction.API.UseCases.Auctions.GetCurrent;
+
+public class GetCurrentAuctionsUseCase
+{
+    public Auction? Execute()
+    {
+        var repository = new RocketseatAuctionDbContext();
+        var today = DateTime.UtcNow;
+
+        return repository
+            .Auctions
+            .Include(auction => auction.Items)
+            .FirstOrDefault(auction => auction.Ends >= today && auction.Starts <= today);
+    }
+}
